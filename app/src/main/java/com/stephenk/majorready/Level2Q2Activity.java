71717b14f84c5975f5c079d2f34f540163b7d38d
Joseph Hinkle <joehinkle11@gmail.com>
2016-01-30 04:17:49 -0500

    spinners on L2Q2 display ATC and G

--- app/src/main/java/com/stephenk/majorready/Level2Q2Activity.java
+++ app/src/main/java/com/stephenk/majorready/Level2Q2Activity.java
@@ -7,7 +7,9 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 
 public class Level2Q2Activity extends AppCompatActivity {
 
@@ -29,6 +31,21 @@ public class Level2Q2Activity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+
+        Spinner spinner1 = (Spinner) findViewById(R.id.L2Q2Spinner1);
+        Spinner spinner2 = (Spinner) findViewById(R.id.L2Q2Spinner2);
+        Spinner spinner3 = (Spinner) findViewById(R.id.L2Q2Spinner3);
+        Spinner spinner4 = (Spinner) findViewById(R.id.L2Q2Spinner4);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.nucleotides, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinner1.setAdapter(adapter);
+        spinner2.setAdapter(adapter);
+        spinner3.setAdapter(adapter);
+        spinner4.setAdapter(adapter);
 
 
     }
