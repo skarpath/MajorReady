6f39e2057d39ccba4acdb604790f3a1e09b7dd14
GabeJason <gablications@gmail.com>
2016-01-30 05:21:27 -0500

    Level 1 Question 4, 5, 6 complete

--- app/src/main/java/com/stephenk/majorready/Level1Q4Activity.java
+++ app/src/main/java/com/stephenk/majorready/Level1Q4Activity.java
@@ -11,19 +11,19 @@ import android.widget.Toast;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Level1Q3Activity extends AppCompatActivity {
+public class Level1Q4Activity extends AppCompatActivity {
 
     Spinner sItems;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_level1_q3);
+        setContentView(R.layout.activity_level1_q4);
 
         List<String> spinnerArray =  new ArrayList<String>();
-        spinnerArray.add("Dominant Frequency times .6");
-        spinnerArray.add("1 - Dominant Frequency");
-        spinnerArray.add("Dominant Frequency - Recessive Frequency");
+        spinnerArray.add("Dominant Frequency ^2");
+        spinnerArray.add("2 * Dominant Frequency * Recessive Frequency");
+        spinnerArray.add("Recessive Frequency ^2");
 
         ArrayAdapter<String> adapter = new ArrayAdapter<>(
                 this, android.R.layout.simple_spinner_item, spinnerArray);
@@ -33,10 +33,10 @@ public class Level1Q3Activity extends AppCompatActivity {
         sItems.setAdapter(adapter);
     }
 
-    public void Lvl1Q3nextBtn(View view){
+    public void Lvl1Q4nextBtn(View view){
         String selected = sItems.getSelectedItem().toString();
-        if (selected.equals("1 - Dominant Frequency")) {
-            Intent intent = new Intent(this, Level1Q4Activity.class);
+        if (selected.equals("Dominant Frequency ^2")) {
+            Intent intent = new Intent(this, Level1Q5Activity.class);
             startActivity(intent);
         }else{
             Toast toast = Toast.makeText(getApplicationContext(),"Incorrect answer",Toast.LENGTH_LONG);
