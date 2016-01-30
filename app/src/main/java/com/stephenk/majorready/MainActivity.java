db4a95cf992388a6643e9d5d0832e3feacd13ff1
Stephen <skarpath@gmail.com>
2016-01-29 23:35:00 -0500

    Added background and first activity

--- app/src/main/java/com/stephenk/majorready/MainActivity.java
+++ app/src/main/java/com/stephenk/majorready/MainActivity.java
@@ -1,5 +1,6 @@
 package com.stephenk.majorready;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,24 +9,21 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.Window;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
+        setContentView(R.layout.main_activity);
+
+
+
+
     }
 
     @Override
