b1a3cc07ab5198c17a83a74dfb54e3f0586bbc6a
Joseph Hinkle <joehinkle11@gmail.com>
2016-01-30 04:26:11 -0500

    validates that your answer is correct on L2Q2

--- app/src/main/java/com/stephenk/majorready/Level2Q2Activity.java
+++ app/src/main/java/com/stephenk/majorready/Level2Q2Activity.java
@@ -10,11 +10,16 @@ import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 public class Level2Q2Activity extends AppCompatActivity {
 
 
     Button Level2Q2Activity;
+    Spinner spinner1;
+    Spinner spinner2;
+    Spinner spinner3;
+    Spinner spinner4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,20 +28,10 @@ public class Level2Q2Activity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-
-        Level2Q2Activity = (Button) findViewById(R.id.btnNextL2Q2);
-        Level2Q2Activity.setOnClickListener(new Button.OnClickListener() {
-            public void onClick(View v) {
-                Intent intent = new Intent(Level2Q2Activity.this, Level2Q3Activity.class);
-                startActivity(intent);
-            }
-        });
-
-
-        Spinner spinner1 = (Spinner) findViewById(R.id.L2Q2Spinner1);
-        Spinner spinner2 = (Spinner) findViewById(R.id.L2Q2Spinner2);
-        Spinner spinner3 = (Spinner) findViewById(R.id.L2Q2Spinner3);
-        Spinner spinner4 = (Spinner) findViewById(R.id.L2Q2Spinner4);
+        spinner1 = (Spinner) findViewById(R.id.L2Q2Spinner1);
+        spinner2 = (Spinner) findViewById(R.id.L2Q2Spinner2);
+        spinner3 = (Spinner) findViewById(R.id.L2Q2Spinner3);
+        spinner4 = (Spinner) findViewById(R.id.L2Q2Spinner4);
         // Create an ArrayAdapter using the string array and a default spinner layout
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.nucleotides, android.R.layout.simple_spinner_item);
         // Specify the layout to use when the list of choices appears
@@ -46,6 +41,20 @@ public class Level2Q2Activity extends AppCompatActivity {
         spinner2.setAdapter(adapter);
         spinner3.setAdapter(adapter);
         spinner4.setAdapter(adapter);
+
+
+        Level2Q2Activity = (Button) findViewById(R.id.btnNextL2Q2);
+        Level2Q2Activity.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View v) {
+                if ((spinner1.getSelectedItemPosition()==1)&&(spinner2.getSelectedItemPosition()==0)&&(spinner3.getSelectedItemPosition()==3)&&(spinner4.getSelectedItemPosition()==4)){
+                    Intent intent = new Intent(Level2Q2Activity.this, Level2Q3Activity.class);
+                    startActivity(intent);
+                }else{
+                    Toast.makeText(Level2Q2Activity.this, "Incorrect, try again", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
 
 
     }
